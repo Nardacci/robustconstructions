90f15ff0160fb4856bface86859a6fc368caa9b8
André Nardacci <56738656+Nardacci@users.noreply.github.com>
2026-08-25 15:47:46 -0300

    Add files via upload

---
+++ style.css
@@ -0,0 +1,15 @@
+/* Ocultar elementos desnecessários na hora de gerar PDF ou Imprimir */
+@media print {
+  body {
+    background: white !important;
+    padding: 0 !important;
+  }
+  button, select {
+    border: none !important;
+    background: transparent !important;
+    appearance: none !important;
+  }
+  .shadow-xl, .shadow-lg, .shadow-sm {
+    box-shadow: none !important;
+  }
+}
